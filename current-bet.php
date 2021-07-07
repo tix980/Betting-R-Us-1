8cd9811793efb67e3185ffcfd5ca9d67171380f2
Natasha Chambers <natashaagathachambers@gmail.com>
2021-07-06 20:41:51 -0400

    made changes to navigation

--- current-bet.php
+++ current-bet.php
@@ -3,16 +3,17 @@
 <head>
     <meta charset="utf-8">
     <title>Current Bets</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <link  rel="stylesheet" type="text/css" href="css/contact-bet-history.css">
     <link  rel="stylesheet" type="text/css" href="css/main.css">
     <meta name="viewport" content="width=device-width">
 </head>
 <body>
-<?php
-include 'header.php';
-?>
+<?php include 'header.php'; ?>
 
+<main id="main">
 <section id="current-bet">
     <div class="current-bet-heading">
         <h2>Current on-going Bets</h2>
@@ -63,6 +64,7 @@ include 'header.php';
 
 
 </section>
+</main>
 
 
 
