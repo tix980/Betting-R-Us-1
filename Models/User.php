bcc9a668b2d069203cb01ab24d1813285976ffad
Natasha Chambers <natashaagathachambers@gmail.com>
2021-08-04 15:44:18 -0400

    add and delete for users table now working

--- Models/User.php
+++ Models/User.php
@@ -25,47 +25,47 @@
         }
 
         // Add a User to the database
-        // public function addUser($username, $firstname, $lastname, $email, $password, $dob, $user_since, $db) {
-        //     $query = "INSERT INTO users (username, firstname, lastname, email, user_password, date_of_birth, user_since) VALUES (:username, :firstname, :lastname, :email, :user_password, :dob, :user_since)";
-        //     $pdostm = $db->prepare($query);
+        public function addUser($username, $firstname, $lastname, $email, $password, $membership, $dob, $db) {
+            $query = "INSERT INTO users (username, firstname, lastname, email, user_password, membership, date_of_birth, user_since) VALUES (:username, :firstname, :lastname, :email, :user_password, :membership, :dob, NOW())";
+            $pdostm = $db->prepare($query);
 
-        //     $pdostm->bindParam(':username', $username);
-        //     $pdostm->bindParam(':firstname', $firstname);
-        //     $pdostm->bindParam(':lastname', $lastname);
-        //     $pdostm->bindParam(':email', $email);
-        //     $pdostm->bindParam(':user_password', $password);
-        //     $pdostm->bindParam(':dob', $dob);
-        //     $pdostm->bindParam(':user_since', $user_since);
+            $pdostm->bindParam(':username', $username);
+            $pdostm->bindParam(':firstname', $firstname);
+            $pdostm->bindParam(':lastname', $lastname);
+            $pdostm->bindParam(':email', $email);
+            $pdostm->bindParam(':user_password', $password);
+            $pdostm->bindParam(':membership', $membership);
+            $pdostm->bindParam(':dob', $dob);
 
-        //     $count = $pdostm->execute();
+            $count = $pdostm->execute();
 
-        //     return $count;
-        // }
+            return $count;
+        }
 
         // Update a User within the database
-        // public function updateUser($id, $username, $firstname, $lastname, $email, $password, $dob, $db) {
-        //     $query = "UPDATE users SET username = :username, firstname = :firstname, lastname = :lastname, email = :email, user_password = :user_password, date_of_birth = :dob WHERE id = :id";
+        public function updateUser($id, $username, $firstname, $lastname, $email, $password, $dob, $db) {
+            $query = "UPDATE users SET username = :username, firstname = :firstname, lastname = :lastname, email = :email, user_password = :user_password, date_of_birth = :dob WHERE id = :id";
 
-        //     $pdostm = $db->prepare($query);
+            $pdostm = $db->prepare($query);
 
-        //     $pdostm->bindParam(':username', $username);
-        //     $pdostm->bindParam(':firstname', $firstname);
-        //     $pdostm->bindParam(':lastname', $lastname);
-        //     $pdostm->bindParam(':email', $email);
-        //     $pdostm->bindParam(':user_password', $password);
-        //     $pdostm->bindPara(':dob', $dob);
-        //     $pdostm->bindParam(':id', $id);
+            $pdostm->bindParam(':username', $username);
+            $pdostm->bindParam(':firstname', $firstname);
+            $pdostm->bindParam(':lastname', $lastname);
+            $pdostm->bindParam(':email', $email);
+            $pdostm->bindParam(':user_password', $password);
+            $pdostm->bindPara(':dob', $dob);
+            $pdostm->bindParam(':id', $id);
 
-        //     $count = $pdostm->execute();
-        //     return $count;
-        // }
+            $count = $pdostm->execute();
+            return $count;
+        }
 
         // Delete a User from the database
         public function deleteUser($id, $db) {
             $query = "DELETE FROM users WHERE id = :id";
 
             $pdostm = $db->prepare($query);
-            $pdostm->bindParam(':id', $id);
+            $pdostm->bindParam(":id", $id);
 
             $count = $pdostm->execute();
 
--- register.php
+++ register.php
@@ -7,14 +7,14 @@
         $username = $_POST['username'];
         $firstname = $_POST['firstname'];
         $lastname = $_POST['lastname'];
-        $dob = $_POST['dob'];
         $email = $_POST['email'];
         $password = $_POST['user_password'];
-        $user_since = date("Y-m-d");
+        $membership = $_POST['membership'];
+        $dob = $_POST['date_of_birth'];
 
         $db = Database::getDb();
         $userObj = new User();
-        $count = $userObj->addUser($username, $firstname, $lastname, $email, $password, $dob, $db);
+        $count = $userObj->addUser($username, $firstname, $lastname, $email, $password, $membership, $dob, $db);
 
         if($count) {
             header("Location: Users/list_users.php");
@@ -54,10 +54,6 @@
                         <input id="lastname" type="text" name="lastname" />
                     </div>
                     <div class="formFields">
-                        <label for="dob">Date of Birth:</label>
-                        <input id="dob" type="date" name="date_of_birth" />
-                    </div>
-                    <div class="formFields">
                         <label for="email">Email:</label>
                         <input id="email" type="email" name="email" />
                     </div>
@@ -65,6 +61,18 @@
                         <label for="password">Password</label>
                         <input id="password" type="password" name="user_password" />
                     </div>
+                    <div class="formFields">
+                        <label for="user_membership">Membership:</label>
+                        <input id="user_membership" type="text" name="membership" />
+                    </div>
+                    <div class="formFields">
+                        <label for="dob">Date of Birth:</label>
+                        <input id="dob" type="date" name="date_of_birth" />
+                    </div>
+                    <!-- <div class="formFields">
+                        <label for="userSince">User Since:</label>
+                        <input id="userSince" type="date" name="user_since" />
+                    </div> -->
                     <button class="profileBtn" type="submit" name="addUser">Register</button>
                 </form>
             </div>
