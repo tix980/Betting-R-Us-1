91aec79ffbbff0462718b9d9909ada2b99e1bb80
Neil Moras <neilmoras@gmail.com>
2021-08-13 18:22:25 -0400

    added comment

--- list-actors.php
+++ list-actors.php
@@ -6,7 +6,7 @@ use BettingRUs\Models\{Database, MovieInfo};
 require_once "vendor/autoload.php";
 //require_once "Models/Database.php";
 //require_once "Models/MovieInfo.php";
-//added comment
+//added
 (string)$userType = $_SESSION['accounttype'];
 if($userType == 'admin') {
     $adminBtn = "style='display:block;'";
